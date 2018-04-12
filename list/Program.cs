209bba0792bd5b9a259b60c1f908ee3b37684de1
m305-57\w401 <charlotte0388@gmail.com>
2018-04-12 17:40:58 +0800

    2

--- list/Program.cs
+++ list/Program.cs
@@ -12,18 +12,6 @@ namespace list
         {
             Random rand = new Random();
 
-            for (int i = 0; i < 6; i++)
-            {
-                int r0 = rand.Next(1, 43);
-                int r1 = rand.Next(1, 43);
-                int r2 = rand.Next(1, 43);
-                int r3 = rand.Next(1, 43);
-                int r4 = rand.Next(1, 43);
-                int r5 = rand.Next(1, 43);
-
-                Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r0, r1, r2, r3, r4, r5);
-            }
-
             List<int> numbers = new List<int>();
 
             for (int i = 1; i <= 43; i++)
