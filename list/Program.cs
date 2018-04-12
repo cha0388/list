c63f6f7633ef3f1a98b10940742d9c4f26fbd79d
m305-57\w401 <charlotte0388@gmail.com>
2018-04-12 17:57:09 +0800

    3, 加註解

--- list/Program.cs
+++ list/Program.cs
@@ -10,18 +10,28 @@ namespace list
     {
         static void Main(string[] args)
         {
+            //設名為rand的亂數
             Random rand = new Random();
 
+            //產生名為number的list
             List<int> numbers = new List<int>();
 
+            //在number裡加入數字1-42
             for (int i = 1; i <= 43; i++)
             {
                 numbers.Add(i);
             }
+
+            //執行六次
             for (int i = 0; i < 6; i++)
             {
+                //產生名為r的數(範圍:1-number的數量)
                 int r = rand.Next(1, numbers.Count);
+
+                //寫下number中的第r個數字
                 Console.WriteLine(numbers[r]);
+
+                //刪除已寫過的數字
                 numbers.RemoveAt(r);
             }
         
